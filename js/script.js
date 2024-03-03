99e6f4bc233af30b1075f60b88f98f10685b270b
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-03 15:42:31 +0600

    add product to cart and add a remove button to remove. Successfully complete the project

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
-let productBtn = document.querySelector('#Watch-btn');
+let productBtn = document.querySelector('#product-list');
+let cartList = document.querySelector('#cart-list');
+
 
 
 //add eventlistner:
 productBtn.addEventListener('click', AddToCart);
-
+cartList.addEventListener('click', removeProductFromCart);
 
 
 // Functions
 function AddToCart(e){
-    // console.log("function called");
-    if (e.hasAtrribute('Watch-btn')){
-        console.log("worked");
+    if(e.target.hasAttribute("href")){
+        // console.log("watch btn clicked")
+        let product = e.target.parentElement;
+        let ul = document.createElement('ul');
+        let li = document.createElement('li');
+        li.textContent = product.querySelector('p').textContent + ' ' + product.querySelector('p:nth-child(2)').textContent;
+
+
+        let link = document.createElement('a');
+        link.setAttribute('href','#');
+        link.innerHTML = '      X';
+        // console.log(link)
+
+        li.appendChild(link);
+        ul.appendChild(li);
+
+        let cartList = document.querySelector('#cart-list');
+        cartList.appendChild(ul);
+
+    }   
+    e.preventDefault();
+}
+
+
+function removeProductFromCart(e){
+    if(e.target.hasAttribute("href")){
+        if(confirm("Are you sure?")) {
+            e.target.parentElement.remove();
+        }
     }
-    let product = productBtn.parentElement;
-    let cartList = document.querySelector('#cart-list');
-    let li = document.createElement('li');
-    li.appendChild(product);
-    let ol = document.createElement('ol');
-    ol.appendChild(li);
-    cartList.appendChild(ol);
+
     e.preventDefault();
 }
